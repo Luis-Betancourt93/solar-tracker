e390447fc0786bab586962c137c4bc8b406e4c76
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-10-19 22:19:12 -0700

    Starting files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}, better go catch it!`)
+  })
